c950f9976c2d86b050d408e8a34664a7fcea2045
tatsuro murata <tatsuro@tatsuronoMacBook-Pro.local>
2021-06-03 19:09:24 +0900

    function for mongodb

--- models/models_mongodb_data_type.go
+++ models/models_mongodb_data_type.go
@@ -4,3 +4,10 @@ type mongoDataType struct {
 	Rid     string `bson:"rid"`
 	Keyword string `bson:"keyword"`
 }
+
+func NewMongoDataType(rid string, keyword string) *mongoDataType {
+	return &mongoDataType{
+		Rid:     rid,
+		Keyword: keyword,
+	}
+}
